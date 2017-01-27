975d5897111a4357eff2bba3ef7b638e803877d7
John Ambrosiano <ambro@lanl.gov>
2017-01-27 14:47:21 -0700

    Tested scraper flow.

--- gmd-python/supermag/scraper_test.py
+++ gmd-python/supermag/scraper_test.py
@@ -1,22 +1,50 @@
 import requests
-from scraper.supermag import StdQuery
+
+from supermag.query import SupermagParams
+from supermag.queryutils import StationParam, IntervalParam
+
+from datetime import datetime
 
 
 if __name__=="__main__":
-    q = StdQuery()
 
-    print("data=" + str(q.createParamsDict()))
+    startdate = datetime(day=1,month=1,year=2000)
+    enddate = datetime(day=2,month=1,hour=1,year=2000)
 
-    r = requests.get(q.host, proxies=q.proxydict, params=q.createParamsDict(), stream=True)
+    sparam = StationParam()
+    stations = sparam.stations
+    ns = len(stations)
+    ind = list(range(0,ns,100))
+    ind.append(ns)
+    print(ind)
 
-    print("Status="+str(r.status_code))
-    print(dict(r.headers))
+    iparam = IntervalParam()
+    interval = iparam.createIntervalString(startdate,enddate)
+    startparam = startdate.isoformat() + '.000Z'
 
     filename = './supermag_test.csv'
+    newline ='\n'
 
     with open(filename, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=1024):
-            fd.write(chunk)
+        for i in range(0,len(ind)-1):
+            print('stations '+str(ind[i])+':'+str(ind[i+1]-1))
+            sstations = sparam.createStationString(ind[i],ind[i+1]-1)
 
+            p = SupermagParams(startparam, interval, sstations)
+            pdict = p.createParamsDict()
+
+            print("params=" + str(pdict))
+
+            r = requests.get(p.host, proxies=p.proxydict, params=pdict, stream=True)
+            print("Status="+str(r.status_code))
+            print(dict(r.headers))
+
+            first = True
+            for line in r.iter_lines():
+                if first:
+                    first=False
+                else:
+                    fd.write(line)
+                    fd.write(newline.encode())
     fd.close()
 
